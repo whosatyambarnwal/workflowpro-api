b9379feb74350beeaf041b2674dc9bff39abacb1
Satyam Barnwal <whosatyambarnwal@gmail.com>
2026-03-24 21:29:24 +0530

    created and registered api.php file
    in Latest version we do not have the api.php file bydefault so we need to create the api.php file manually and need to register them inside the bootstrap/app.php under routing section with path ( same as the web.php )

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\AuthController;
+
+Route::prefix('v1')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
+});
